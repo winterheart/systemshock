0fa50585755bcd2eab36d68a4a170b0abfe9e418
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2020-02-25 23:36:42 +0300

    Add experimental support of conan manager

---
+++ conanfile.py
@@ -0,0 +1,31 @@
+from conans import ConanFile, CMake
+
+class SystemShockConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    # Comma-separated list of requirements
+    build_requires = [
+        "sdl2_mixer/2.0.4@bincrafters/stable",
+    ]
+    generators = "cmake_find_package"
+
+    def configure(self):
+        # Disable unused dependencies
+        self.options["sdl2"].shared = True
+
+        self.options["sdl2"].jack = False
+        self.options["sdl2"].nas = False
+        self.options["sdl2"].pulse = False
+        self.options["sdl2_mixer"].flac = False
+        self.options["sdl2_mixer"].fluidsynth = False
+        self.options["sdl2_mixer"].mad = False
+        self.options["sdl2_mixer"].mikmod = False
+        self.options["sdl2_mixer"].modplug = False
+        self.options["sdl2_mixer"].mpg123 = False
+        self.options["sdl2_mixer"].ogg = False
+        self.options["sdl2_mixer"].opus = False
+        self.options["sdl2_mixer"].tinymidi = False
+
+
+    def imports(self):
+        self.copy("*.dll", "", "bin")
+        self.copy("*.dylib*", "", "lib")
